9d7f0ec0b1f7662719e6ab98f6ed960254679906
NicolasBoni <rm551965@fiap.com.br>
2023-03-16 02:33:35 -0300

    Alteração de pastas e JS adicionado.

--- rm551965/js/script.js
+++ rm551965/js/script.js
@@ -1,7 +1,7 @@
 //Executando um teste 
 
 //Declarando uma variável e visualizando seu conteudol.
-var nome = 'Nicolas'
+var nome ='Nicolas'
 
-document.write('O valor da variável')
+document.write('O valor da variável') 
 document.write(nome)
