5fca44366dfbf1d898e363b020af9e397f73f058
ricardoakira <rm551447@fiap.com.br>
2023-03-20 09:55:58 -0300

    Declaraçã de variáveis + escopo let var.

--- rm551447/js/script.js
+++ rm551447/js/script.js
@@ -1,8 +1,28 @@
-//Executando um teste
-
+//Comentário de Linha
+/**
+ * Comentário de Bloco
+ */
 //Declarando uma variável e visualizando seu conteúdo.
-var nome = "Filipe"
+var nome = "Ricardo"
 
-document.write("O valor da variável.")
-document.write(nome)
+Document.write("O valor da variável")
+//Realizando uma concatecação
+Document.write(nome)
+Document.write("NOME : " + nome)
 
+//Três tipos de dados do var
+//undefined
+//var tipo1
+//nulo
+var tipo2 = null
+//Vazio
+var tipo3 = ""
+
+//Problemas ao declarar tudo com var
+var nome = "Pedro"
+
+if(nome != ""){
+   let nome = Rony
+}   
+)
+Console.log("NOME : " + nome)
