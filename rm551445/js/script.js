be830cae3b8d3028bb420b535376ff7c76c93b67
Valotto18 <rm551445@fiap.com.br>
2023-03-20 09:55:16 -0300

    Declaração de Variáveis

--- rm551445/js/script.js
+++ rm551445/js/script.js
@@ -1,8 +1,15 @@
 //Executando um teste
+// comentario de linha
+/** 
+* Comentario de Bloco
+*/
 
 //Declarando uma variável e visualizando seu conteúdo.
-var nome = "Filipe"
+var nome = ", Valotto"
 
-document.write("O valor da variável.")
+document.write("Aqui está o link para o site da FIAP")
 document.write(nome)
+// Realizano uma concatenação
+
+alert("Olá, Mundo")
 
