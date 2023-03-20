fbe61128316409ed76ed8476a52e40d7ca0bd3f6
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-03-20 09:55:51 -0300

    Declaração de variaveis + escopo let var.

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -1,8 +1,31 @@
-//Executando um teste
+//Comentário de Linha
+/**
+ * Comentário de Bloco
+ */
+
+
+// document.write("O valor da variável.")
+// document.write("<br>")
+// //Realizando uma concatenação
+// document.write("NOME : " + nome)
 
 //Declarando uma variável e visualizando seu conteúdo.
+// var nome
+
+//Três tipos de dados iniciais do var
+
+//undefined
+var tipo1
+//Nulo
+var tipo2 = null
+//Vazio
+var tipo3 = ""
+
+//Problemas ao delcarar tudo com var
 var nome = "Pedro"
 
-document.write("O valor da variável.")
-document.write(nome)
+if(nome != ""){
+    let nome = "Rony"
+}
 
+console.log("NOME : " + nome)
