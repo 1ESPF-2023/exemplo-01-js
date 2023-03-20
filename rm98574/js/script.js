7e496702297b5eea9a76eefab32761ed49f2e6a4
DriLopess <rm98574@fiap.com.br>
2023-03-20 09:55:15 -0300

    Declaração de variaveis

--- rm98574/js/script.js
+++ rm98574/js/script.js
@@ -1,6 +1,11 @@
-//Executando um teste 
-
+//Coemntário de Linha
+/**
+ * Comentario de bloco
+ */
 // Declarando uma variável e visualizando seu conteúdo.
 var nome = "Adriano"
 document.write("O valor da variável.")
-document.write(nome)
+document.write("<br>")
+//Realizando uma concatenação
+document.write(" Nome: " + nome)
+
