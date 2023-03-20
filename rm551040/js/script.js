cfee4c7743fd4a9dea86aa3bd8db2b81ef75b7f1
lucasvital22 <rm551040@fiap.com.br>
2023-03-20 09:55:53 -0300

    Declaração de variaveis + escopo let var.

--- rm551040/js/script.js
+++ rm551040/js/script.js
@@ -1,8 +1,18 @@
-//Executando um teste
+//Comentario de linha
+/**
+ * comentario de bloco
+ */
 
 //Declarando uma variável e visualizando seu conteúdo.
-var nome = "Lucas"
+   var nome = "Lucas"
 
-document.write("O valor da variável.")
-document.write(nome)
+//    document.write("O valor da variável.")
+//    document.write("<br>")
+//    //Realizando uma concatenação
+//    document.write('NOME: ' + nome)
 
+
+if(nome != ""){
+    let nome = "Sauce"
+}
+console.log("NOME : " + nome)
