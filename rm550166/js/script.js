f2a70ed711793d0097b372b81441f13ef0595053
ricardoVergani <rm550166@fiap.com.br>
2023-03-20 09:56:02 -0300

    declaraçao de variaveis + escopo let e var

--- rm550166/js/script.js
+++ rm550166/js/script.js
@@ -3,5 +3,25 @@
 //Declarando uma variável e visualizando seu conteúdo.
 var nome = "Ricardo"
 
-document .write("o valor da variável")
-document.write(nome)
+// document .write("o valor da variável")
+// document.write("<br>")
+// //realizando uma concatenação
+// document.write("Nome:" + nome)
+
+//tres tipos de dados iniciais dp var
+// undefined
+var tipo1
+// nulo
+var tipo2 = null
+// vazio
+var tipo3 = ""
+
+//problemas ao declarar tudo com var
+var nome = "pedro"
+
+if(nome != ""){
+        var nome = "rony"
+}
+console.log("nome:" + nome)
+
+
