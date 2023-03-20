f01d48a978031f15145629f3f7996570139e5508
KauePastori <rm98501@fiap.com.br>
2023-03-20 09:55:54 -0300

    Declaração de variaveis + escopo let var.

--- rm98501/js/script.js
+++ rm98501/js/script.js
@@ -1,8 +1,46 @@
 //Executando um teste
 
 //Declarando uma variável e visualizando seu conteúdo.
-var nome = "Kaue"
 
-document.write("O valor da variável.")
-document.write(nome)
+//Comentário de linha
+
+/**
+ * Comentário de bloco
+ */
+
+//3 tipos de dados inciais no var
+//undefined
+var tipo1   
+//null
+var tipo2 = null 
+//Vazio 
+var tipo3 = ""  
+
+//Problemas ao declarar tudo com var:
+var nome = "Kaue"
+if(nome != ""){
+    let nome = "Pedro"
+}
+
+console.log("NOME :" + nome)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var nome = "Kaue"
+
+// document.write("O valor da variável.")
+// //Realizando uma concatenação 
+// document.write("NOME: " + nome);
 
