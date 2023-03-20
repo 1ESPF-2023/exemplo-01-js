5204cbc7c3e42a901703ee57cbaea5d3001857bb
Breqsz <rm97974@fiap.com.br>
2023-03-20 09:55:12 -0300

    Declaração de variaveis + escopo let var.

--- rm97974/js/script.js
+++ rm97974/js/script.js
@@ -1,8 +1,28 @@
-//Executando um teste 
-
+//Comentário de linha
+/**
+ * Comentário de Bloco.
+ */
 //Declarando uma variável e visualizando seu conteúdo.
 var nome = "Pedro"
 
 document.write("O valor da variável.")
-document.write(nome)
+document.write("<br>")
+//Realizando concatenação
+document.write("NOME : " + nome)
 
+//Três tipos de dados iniciais do var
+
+//undefined
+var tipo1
+//Nulo
+var tipo2 = null
+//Vazio
+var tipo3 = ""
+
+//Problemas ao declarar tudo com var
+var nome = "Pedro"
+
+if(nome !=""){
+    let nome = "Rony"
+}
+console.log("NOME : " + nome)
