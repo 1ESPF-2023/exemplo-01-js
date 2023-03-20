cccab0b932a322a91352b3042b796d5660ba643d
tavares97877 <rm97877@fiap.com.br>
2023-03-20 09:55:10 -0300

    Declaração de variáveis + escopo let e var em JS

--- rm97877/js/script.js
+++ rm97877/js/script.js
@@ -1,7 +1,31 @@
 // Executando um teste
-
+/**
+ * Comentário de BLOCO
+ * BLOCO
+ * COMENTáRIO DE BLOCO
+ */
 // Declarando uma variável e visualizando seu conteúdo.
+
+
+
+// var nome = "Pedro"
+// document.write("O valor da variável.")
+// document.write(nome)
+// document.write("NOME: " + nome)
+
+// tres tipos de dados iniciais do VAR
+// undefined
+var tipo1
+//Nulo
+var tipo2 = null
+//Vazio
+var tipo3 = ""
+// problemas ao declarar tudo com VAR
 var nome = "Pedro"
 
-document.write("O valor da variável.")
-document.write(nome)
+if(nome != ""){
+
+var nome = "Rony"
+
+}
+    console.log("NOME: " + nome)
