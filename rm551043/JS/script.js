a71cb7dcd648f87e05cbb37794bd25c061675fe6
pedrogava <rm551043@fiap.com.br>
2023-03-20 21:58:26 -0300

    adição de exercicio no js

--- rm551043/JS/script.js
+++ rm551043/JS/script.js
@@ -6,5 +6,8 @@
 //Delarando uma variavel e visualizando seu conteudo
 var nome = "pedro"
 
-document.write("meu nome é ")
-document.write(nome)
+document.write("meu nome é ", nome)
+
+var x=10
+var y="5"
+console.log(x+y)
