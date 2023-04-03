48b15e3f6302bd28be557576d5b175d807d6f59b
97877 <rm97877>
2023-04-03 10:01:27 -0300

    aula eventos

--- rm97877/js/script.js
+++ rm97877/js/script.js
@@ -30,3 +30,25 @@
 // }
 //     console.log("NOME: " + nome)
 
+//capturar o elemento atraves do id utilizando uma funcao do dom
+const h1element = document.getElementById("meu-titulo");
+//atrelar ou adicionar o evento de click do mouse no elemento:
+h1element.addEventListener("click", ()=>{
+    alert('Agora com super poderes')
+
+})
+
+//Altere a cor de fundo do elmento quando ocorrer a passagem do mouse sobre o elemento!
+//Coloque a cor de fundo na cor vermelha e a cor do texto na cor branca.
+
+h1element.addEventListener("mouseover", ()=>{
+    h1element.style.backgroundColor = "#ff0000";
+    h1element.style.color = "#fff";
+    h1element.style.width = "470px";
+    h1element.style.height = "50px";
+})
+
+h1element.addEventListener("mouseout", ()=>{
+    h1element.style.backgroundColor = "#fff";
+    h1element.style.color = "#000000";
+})
