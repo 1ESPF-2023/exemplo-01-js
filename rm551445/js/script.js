dcad7039445a7856fd7e0f4dc92f4b9d3bf3926c
Valotto18 <rm551445@fiap.com.br>
2023-04-03 09:15:13 -0300

    Adição de Alert in line

--- rm551445/js/script.js
+++ rm551445/js/script.js
@@ -1,15 +1,1 @@
-//Executando um teste
-// comentario de linha
-/** 
-* Comentario de Bloco
-*/
-
-//Declarando uma variável e visualizando seu conteúdo.
-var nome = ", Valotto"
-
-document.write("Aqui está o link para o site da FIAP")
-document.write(nome)
-// Realizano uma concatenação
-
-alert("Olá, Mundo")
-
+let nome = prompt("Informe seu Nome aqui!!!")
