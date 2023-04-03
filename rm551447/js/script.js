0d4aa261e4eb4901b5b8612f0e67ebee4a863315
ricardoakira <rm551447@fiap.com.br>
2023-04-03 09:14:37 -0300

    Evento inline onclick

--- rm551447/js/script.js
+++ rm551447/js/script.js
@@ -3,26 +3,108 @@
  * Comentário de Bloco
  */
 //Declarando uma variável e visualizando seu conteúdo.
-var nome = "Ricardo"
 
-Document.write("O valor da variável")
 //Realizando uma concatecação
-Document.write(nome)
-Document.write("NOME : " + nome)
 
 //Três tipos de dados do var
 //undefined
 //var tipo1
+
 //nulo
-var tipo2 = null
+//var tipo2 = null
 //Vazio
-var tipo3 = ""
+//var tipo3 = ""
+
+//var tipo4 = "DECLAREI"
 
 //Problemas ao declarar tudo com var
-var nome = "Pedro"
+//var nome = "Pedro"
+
+// if(nome != ""){
+//    let nome = "Rony"
+// }   
+// )
+//Console.log("TIPO 1 : " + tipo1)
+//Console.log("TIPO 2 : " + tipo2)
+//Console.log("TIPO 3 : " + tipo3)
+
+/**
+ = / Operador de igual é de atribuição
+ == / Operador de igual 2x é de igualdade(validação)
+ != / Operador excla,ação e igual é difirente ou negação, ele inverte o resultado
+ > / operador maior, verifica se o valor da esquerda é maior do que o da dirita
+ < / operador menor, verifica se o valor da esquerda é menor do que o da dirita
+ >= / operador maior, verifica se o valor da esquerda é maior ou igual do que o da direita
+ <= operador menor ou igual, verifica se o valor da esquerda é menor ou igual do que o da direita
+ 
+ */
+//Estrutura de decisão IF / SE 
+
 
 if(nome != ""){
-   let nome = Rony
-}   
-)
-Console.log("NOME : " + nome)
+console.log("IF DE UMA LINHA COM ELSE")
+}else{
+console.log("AQUI É O ELSE!!")
+}
+
+/**
+ Utilzação de operadores lógico
+ && = e ou AND
+ || = ou ou OR
+ Para estes operadoes vamos utilizar também as estruturas de decisão incrementadas com else if
+
+
+ */
+
+//  var nome = "Alexandre"
+//  var sobreNome = "Pereira"
+
+// if(nome != "" && !sobreNome != "") {
+// console.log("O nome está preenchido!")
+// }else if((nome == undefined &&  nome != null)&& (sobreNome == undefined && sobreNome != null)) {
+//    console.log("O nome não está indefinido!")
+// }else if(nome == null && sobreNome == null) {
+//    console.log("O nome está nulo!")
+// }else if(nome == "" && sobreNome == "") {
+//    console.log("O nome não está preenchido!")
+// }else{
+//       console.log("Ocorreu um problema desconhecido")
+//    }
+
+// Operação ternária
+var num1 = 10;
+var num2 = 5;
+var resultado 
+
+//Utilizando o operador matemático (% de Módulo), vamos obter o resto da operação de divisão de 
+//num1 por num2, podendo descobrir se o numéro é opar ou ímpar
+resultado = (num1%num2) ? "PAR" : "IMPAR";
+console.log("O valor calculado é : " + resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
