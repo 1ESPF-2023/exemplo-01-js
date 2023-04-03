e61fc8f3b066e6521267b3d0f7c1e9caee9d7fea
ricardoakira <rm551447@fiap.com.br>
2023-04-03 10:09:49 -0300

    Eventos do mouse no javascript

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
 
-## Exemplo de variáveis Javascriptesse é do aluno 551752
+## Exemplo de variáveis Javascriptesse é do aluno 551447
 
 
-### Criação de variaáveis em Javascript esse é do aluno 551752
+### Criação de variaáveis em Javascript esse é do aluno 551447
 
 
 
-### Criação de variáveis em Javascript esse é do aluno 551752
+### Criação de variáveis em Javascript esse é do aluno 551447
 
 
--- rm551447/js/script.js
+++ rm551447/js/script.js
@@ -1,91 +1,105 @@
-//Comentário de Linha
-/**
- * Comentário de Bloco
- */
-//Declarando uma variável e visualizando seu conteúdo.
+// //Comentário de Linha
+// /**
+//  * Comentário de Bloco
+//  */
+// //Declarando uma variável e visualizando seu conteúdo.
 
-//Realizando uma concatecação
+// //Realizando uma concatecação
 
-//Três tipos de dados do var
-//undefined
-//var tipo1
+// //Três tipos de dados do var
+// //undefined
+// //var tipo1
 
-//nulo
-//var tipo2 = null
-//Vazio
-//var tipo3 = ""
+// //nulo
+// //var tipo2 = null
+// //Vazio
+// //var tipo3 = ""
 
-//var tipo4 = "DECLAREI"
+// //var tipo4 = "DECLAREI"
 
-//Problemas ao declarar tudo com var
-//var nome = "Pedro"
+// //Problemas ao declarar tudo com var
+// //var nome = "Pedro"
+
+// // if(nome != ""){
+// //    let nome = "Rony"
+// // }   
+// // )
+// //Console.log("TIPO 1 : " + tipo1)
+// //Console.log("TIPO 2 : " + tipo2)
+// //Console.log("TIPO 3 : " + tipo3)
+
+// /**
+//  = / Operador de igual é de atribuição
+//  == / Operador de igual 2x é de igualdade(validação)
+//  != / Operador excla,ação e igual é difirente ou negação, ele inverte o resultado
+//  > / operador maior, verifica se o valor da esquerda é maior do que o da dirita
+//  < / operador menor, verifica se o valor da esquerda é menor do que o da dirita
+//  >= / operador maior, verifica se o valor da esquerda é maior ou igual do que o da direita
+//  <= operador menor ou igual, verifica se o valor da esquerda é menor ou igual do que o da direita
+ 
+//  */
+// //Estrutura de decisão IF / SE 
+
 
 // if(nome != ""){
-//    let nome = "Rony"
-// }   
-// )
-//Console.log("TIPO 1 : " + tipo1)
-//Console.log("TIPO 2 : " + tipo2)
-//Console.log("TIPO 3 : " + tipo3)
-
-/**
- = / Operador de igual é de atribuição
- == / Operador de igual 2x é de igualdade(validação)
- != / Operador excla,ação e igual é difirente ou negação, ele inverte o resultado
- > / operador maior, verifica se o valor da esquerda é maior do que o da dirita
- < / operador menor, verifica se o valor da esquerda é menor do que o da dirita
- >= / operador maior, verifica se o valor da esquerda é maior ou igual do que o da direita
- <= operador menor ou igual, verifica se o valor da esquerda é menor ou igual do que o da direita
- 
- */
-//Estrutura de decisão IF / SE 
-
-
-if(nome != ""){
-console.log("IF DE UMA LINHA COM ELSE")
-}else{
-console.log("AQUI É O ELSE!!")
-}
-
-/**
- Utilzação de operadores lógico
- && = e ou AND
- || = ou ou OR
- Para estes operadoes vamos utilizar também as estruturas de decisão incrementadas com else if
-
-
- */
-
-//  var nome = "Alexandre"
-//  var sobreNome = "Pereira"
-
-// if(nome != "" && !sobreNome != "") {
-// console.log("O nome está preenchido!")
-// }else if((nome == undefined &&  nome != null)&& (sobreNome == undefined && sobreNome != null)) {
-//    console.log("O nome não está indefinido!")
-// }else if(nome == null && sobreNome == null) {
-//    console.log("O nome está nulo!")
-// }else if(nome == "" && sobreNome == "") {
-//    console.log("O nome não está preenchido!")
+// console.log("IF DE UMA LINHA COM ELSE")
 // }else{
-//       console.log("Ocorreu um problema desconhecido")
-//    }
+// console.log("AQUI É O ELSE!!")
+// }
 
-// Operação ternária
-var num1 = 10;
-var num2 = 5;
-var resultado 
-
-//Utilizando o operador matemático (% de Módulo), vamos obter o resto da operação de divisão de 
-//num1 por num2, podendo descobrir se o numéro é opar ou ímpar
-resultado = (num1%num2) ? "PAR" : "IMPAR";
-console.log("O valor calculado é : " + resultado)
+// /**
+//  Utilzação de operadores lógico
+//  && = e ou AND
+//  || = ou ou OR
+//  Para estes operadoes vamos utilizar também as estruturas de decisão incrementadas com else if
 
 
+//  */
 
+// //  var nome = "Alexandre"
+// //  var sobreNome = "Pereira"
 
+// // if(nome != "" && !sobreNome != "") {
+// // console.log("O nome está preenchido!")
+// // }else if((nome == undefined &&  nome != null)&& (sobreNome == undefined && sobreNome != null)) {
+// //    console.log("O nome não está indefinido!")
+// // }else if(nome == null && sobreNome == null) {
+// //    console.log("O nome está nulo!")
+// // }else if(nome == "" && sobreNome == "") {
+// //    console.log("O nome não está preenchido!")
+// // }else{
+// //       console.log("Ocorreu um problema desconhecido")
+// //    }
 
+// // Operação ternária
+// var num1 = 10;
+// var num2 = 5;
+// var resultado 
 
+// //Utilizando o operador matemático (% de Módulo), vamos obter o resto da operação de divisão de 
+// //num1 por num2, podendo descobrir se o numéro é opar ou ímpar
+// resultado = (num1%num2) ? "PAR" : "IMPAR";
+// console.log("O valor calculado é : " + resultado)
+
+// CAPTURAR O ELEMENTO ATRAVÉS DO ID UTILIZANDO UMA FUNÇÃO DO DOM 
+const h1Element = document.getElementById("meu-titulo")
+
+//Atrelar ou adicionar o evento de clique do mouse ao elemento
+h1Element.addEventListener("click",()=>{
+    alert("Agora com superpoderes!")
+})
+
+//Altere a cor de fundo do elemento quando ocorrer a passagem do mouse sobre o o elemento"
+//Coloque a cor de fundo vermelha e a cor de texto na cor branca
+
+h1Element.addEventListener("mouseover",()=>{
+    h1Element.style.backgroundColor = "#ff0000";
+    h1Element.style.color = "#fff";
+})
+h1Element.addEventListener("mouseout",()=>{
+    h1Element.style.backgroundColor = "#fff";
+    h1Element.style.color = "#000000";
+})
 
 
 
