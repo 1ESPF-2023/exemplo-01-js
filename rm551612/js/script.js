3efc9ed59eabdb24db0663a663f2a0b38643d77b
JoaoLucasAraujo <rm551612@fiap.com.br>
2023-04-03 09:16:04 -0300

    evento inline ou click

--- rm551612/js/script.js
+++ rm551612/js/script.js
@@ -1,6 +1,34 @@
-//Executando um teste 
+//Comentário de Linha
+/**
+ * Comentário de Bloco
+ */
 
-// declarando uma variavel e visualizando seu conteudo.
-var nome = "joao"
-document.write("o valor da variavel.")
-document.write (nome)
+
+// document.write("O valor da variável.")
+// document.write("<br>")
+// //Realizando uma concatenação
+// document.write("NOME : " + nome)
+
+//Declarando uma variável e visualizando seu conteúdo.
+// var nome
+
+//Três tipos de dados iniciais do var
+
+//undefined
+var tipo1
+//Nulo
+var tipo2 = null
+//Vazio
+var tipo3 = ""
+
+//Problemas ao delcarar tudo com var
+var nome = "Pedro"
+
+if(nome != ""){
+    let nome = "Rony"
+}
+
+console.log("NOME : " + nome)
+
+
+
