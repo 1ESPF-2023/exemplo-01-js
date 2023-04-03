e95711b1913f9adf99ca6baaf9ab7f1cfe4e5be9
pedrogava <rm551043@fiap.com.br>
2023-04-03 09:53:55 -0300

    stilizando a pagina em JS

--- rm551043/JS/script.js
+++ rm551043/JS/script.js
@@ -4,3 +4,22 @@
  */
 
 //Delarando uma variavel e visualizando seu conteudos 
+// capturar o elemento atraves do id utilizando uma função do DOM
+
+const h1Element = document.getElementById("meu_titulo")
+
+// atrelar ou adicionar o evento de click do mouse ao elemento 
+
+h1Element.addEventListener("click",()=>{
+    alert("agora com super poderes")
+})
+// altere a cor de fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
+// Coloque a cor de fundo na cor vermelha e a cor do texto em branco
+h1Element.addEventListener("mouseover",()=>{
+    h1Element.style.backgroundColor = "#ff0000";
+    h1Element.style.color = "#fff";
+})
+h1Element.addEventListener("mouseout",()=>{
+    h1Element.style.backgroundColor = "#fff";
+    h1Element.style.color = "#000000";
+})
