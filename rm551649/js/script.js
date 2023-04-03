060822dca68ad3e9c27e31d5f19462bb9a065752
vvctort1 <rm551649@fiap.com.br>
2023-04-03 09:54:19 -0300

    exemplo de onmouseover mudando cor fundo cor texto

--- rm551649/js/script.js
+++ rm551649/js/script.js
@@ -1,8 +1,32 @@
-//Executando um teste
+// //Executando um teste
 
-//Declarando uma variável e visualizando seu conteúdo.
-var nome = "Pedro"
+// //Declarando uma variável e visualizando seu conteúdo.
+// var nome = "Pedro"
 
-document.write("O valor da variável.")
-document.write(nome)
+// document.write("O valor da variável.")
+// document.write(nome)
+
+//CAPTURAR O ELEMENTO ATRAVÉS DO ID UTILIZANDO UMA FUNÇÃO DO DOM
+const h1Element = document.getElementById('meu-titulo');
+
+//Atrelar ou adicionar o evento de click do mouse ao elemento.
+h1Element.addEventListener("mouseover",()=>{
+    alert('Agora com superpoderes!');
+})
+
+//Altere a cor de fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
+//Coloque a cor de fundo na cor vermelha e a cor do texto na cor branca.
+
+
+
+h1Element.addEventListener("mouseover", () => {
+    h1Element.style.backgroundColor = '#ff0000'
+    h1Element.style.color = '#fff'
+})
+
+h1Element.addEventListener("mouseout", ()=>{
+    h1Element.style.backgroundColor = '#fff'
+    h1Element.style.color = '#000000'
+})
+
 
