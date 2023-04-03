4c9fef3190d51689e0c896a3492966e8c7d8e1bf
PedroPereiradosSantos37 <rm552047@fiap.com.br>
2023-04-03 09:51:01 -0300

    Trocando com a passagem de mousee do fundo!

--- rm552047/js/script.js
+++ rm552047/js/script.js
@@ -91,3 +91,25 @@
 //resultado = !(num1%num2) ? "PAR" : "IMPAR";
 
 //console.log("O valor calculado é : " + resultado)
+
+//CAPITURAR O ELEMENTO ATRAVÉS DO ID UTILIZANDO UMA FUNÇÃO DO DOM
+const h1Element = document.getElementById("meu-titulo");
+
+//Atrelar ou aidicionar o evento de click do mouse ao elemento.
+h1Element.addEventListener("click",()=>{
+    alert("Agora com superpoderes!")
+})
+
+
+//Altere a cor de fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
+//Coloque a cor de fundo na cor vermelha e a cor  do texto na cor branca.
+
+h1Element.addEventListener("mouseover", ()=>{
+     h1Element.style.backgroundColor = "#ff0000"
+     h1Element.style.color = "#fff";
+})
+
+h1Element.addEventListener("mouseout", ()=>{
+    h1Element.style.backgroundColor = "#fff"
+    h1Element.style.color = "#000000";
+})
