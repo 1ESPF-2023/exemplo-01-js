7d4949e89fe4861841e0f02bbf71697a36aeb8df
pedrogava <rm551043@fiap.com.br>
2023-04-10 09:53:52 -0300

    Collections

--- rm551043/JS/script.js
+++ rm551043/JS/script.js
@@ -1,25 +1,64 @@
-//comentario de linha
-/**
- * comentario de bloco
- */
+// //comentario de linha
+// /**
+//  * comentario de bloco
+//  */
 
-//Delarando uma variavel e visualizando seu conteudos 
-// capturar o elemento atraves do id utilizando uma função do DOM
+// //Delarando uma variavel e visualizando seu conteudos 
+// // capturar o elemento atraves do id utilizando uma função do DOM
 
-const h1Element = document.getElementById("meu_titulo")
+// const h1Element = document.getElementById("meu_titulo")
 
-// atrelar ou adicionar o evento de click do mouse ao elemento 
+// // atrelar ou adicionar o evento de click do mouse ao elemento 
 
-h1Element.addEventListener("click",()=>{
-    alert("agora com super poderes")
-})
-// altere a cor de fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
-// Coloque a cor de fundo na cor vermelha e a cor do texto em branco
-h1Element.addEventListener("mouseover",()=>{
-    h1Element.style.backgroundColor = "#ff0000";
-    h1Element.style.color = "#fff";
-})
-h1Element.addEventListener("mouseout",()=>{
-    h1Element.style.backgroundColor = "#fff";
-    h1Element.style.color = "#000000";
-})
+// h1Element.addEventListener("click",()=>{
+//     alert("agora com super poderes")
+// })
+// // altere a cor de fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
+// // Coloque a cor de fundo na cor vermelha e a cor do texto em branco
+// h1Element.addEventListener("mouseover",()=>{
+//     h1Element.style.backgroundColor = "#ff0000";
+//     h1Element.style.color = "#fff";
+// })
+// // h1Element.addEventListener("mouseout",()=>{
+// //     h1Element.style.backgroundColor = "#fff";
+// //     h1Element.style.color = "#000000";
+// // })
+
+// h1Element.addEventListener("mouseout",)
+// function alteraCorFundo(){
+//     h1Element.style.backgroundColor = "#fff";
+//     h1Element.style.color = "#000000";
+// }
+// if(condition) {
+
+// }
+// let msg = "mensagem enviada de longe!"
+// function AlertaColorfundo (msg){
+//     //Parametro recebido e sendo verificado
+//     // SWITCH CASE
+//     const pElemente = document.getElementById("p-msg");
+//     switch (msg) {
+//         case undefined:
+//             console.log ( " foi enviado uma mensagem vazia");
+//             pElement.textContent(" foi enviado uma mensagem vazia");    
+//             break;
+//         case null:
+//             console.log("foi enviada uma mensagem nula");
+//             pElement.textContent(" foi enviada uma mensagem nula");
+//             break;
+//         case "":
+//             console.log("foi enviada uma mensagem em branco");
+//             pElement.textContent("foi enviada uma mensagem em branco");
+//             break;
+//         case "bom dia amigos":
+//             console.log("bom dia ammigos");
+//             pElement.textContent("bom dia amigos");
+//             break;
+//             default:
+//             console.error("ocOrreu um erro");
+//             pElement.textContent("ocorreu um erro na transmisão")
+//             break;
+//     }
+// return"SUCESSO"
+// }
+// console.log("Esse é o retorno para a mnha função: " + enviaMsg(null))
