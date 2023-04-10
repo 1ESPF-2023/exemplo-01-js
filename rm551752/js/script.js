9a7ba2fb1378e15fca643097bf5a8f04019b9065
DRAFESS <rm551752@fiap.com.br>
2023-04-10 09:42:50 -0300

    commit 4/10

--- rm551752/js/script.js
+++ rm551752/js/script.js
@@ -1,6 +1,41 @@
 //executando um teste 
 
 //declarando uma variavel e visualizando seu conteudo.
-var nome = "Diogo"
-document.write("o valor da variavel.")
-document.write(nome)
+
+h1element.addEventListener("mouseout", alteraCorFundo )
+
+let msg = "bom dia amios!"
+
+function alteraCorFundo(msg){
+    //parametro recebido e sendo verificado 
+    //switch case
+    
+    const pElement = document.getElementById("p-msg");
+
+    switch (msg) {
+        case undefined:
+            console.log("foi enviada uma mensagem vazia");
+            pElement.textContent="foi enviada uma mensagem vazia"
+
+            break;
+         case null:
+                console.log("foi enviada uma mensagem nula");
+                pElement.textContent="foi enviada uma mensagem nula"
+                break;
+            case "":
+                    console.log("foi enviada uma mensagem em branco");
+                    pElement.textContent ="foi enviada uma mensagem branco"
+                break;
+              case "bom dia amigos !":
+                        console.log("bom dia amigos !");
+                        pElement.textContent="bom dia amigos !"
+        default:
+            console.error("ocorreu um erro na transmissão")
+            pElement.textContent="ocorreu um erro na transmissão"
+            break;
+    }
+
+    return "sucesso!";
+}
+console.log("esse é o retorno da minha função")
+enviaMsg(null)
