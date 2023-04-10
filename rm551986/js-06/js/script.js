1f3819bd0b86055c39f7405c82786864f0ed8cc9
G3n4r00 <rm551986@fiap.com.br>
2023-04-10 09:40:29 -0300

    Functions/switch case/return/param

--- rm551986/js-06/js/script.js
+++ rm551986/js-06/js/script.js
@@ -44,7 +44,59 @@ h1Element.addEventListener("mouseover",() =>{
 
 })
 
-h1Element.addEventListener("mouseout" ,() =>{
+// h1Element.addEventListener("mouseout" ,() =>{
+//     h1Element.style.backgroundColor = "#fff";
+//     h1Element.style.color = "#000";
+// })
+
+h1Element.addEventListener("mouseout", alteraCorFundo) 
+
+function alteraCorFundo(){
     h1Element.style.backgroundColor = "#fff";
     h1Element.style.color = "#000";
-})
+}
+
+// let msg = undefined
+
+
+// function enviaMsg(msg){
+
+//     //Parametro recebido e sendo verificad
+//     //SWITCH CASE
+
+//     const pElement = document.getElementById("p-msg");
+
+//     switch (msg) {
+//         case undefined:
+//             console.log("Foi enviada uma mensagem vazia!")
+//             pElement.textContent = "Foi enviada uma mensagem vazia!"
+//             return "Foi enviada uma mensagem vazia!";
+//         case null:
+//                 console.log("Foi enviada uma mensagem nula!")
+//                 pElement.textContent = "Foi enviada uma mensagem nula!"
+//             return "Foi enviada uma mensagem nula!";
+//             case "":
+//                 console.log("Foi enviada uma mensagem em branco!")
+//                 pElement.textContent = "Foi enviada uma mensagem em branco!"
+//             return "Foi enviada uma mensagem em branco!";
+//         case "Bom dia amigos!":
+//                 console.log("Bom dia amigo!")
+//                 pElement.textContent = "Bom dia amigo!"
+//             return "Bom dia amigo!";            
+//         default:
+//                 console.error("Ocorreu um erro na transmissão")
+//                 pElement.textContent = "Ocorreu um erro na transmissão"
+//             return "Ocorreu um erro na transmissão";                        
+//     }
+
+
+//     return "SUCESSO!";
+//     // Esse cara foi moto pq ele nunca vai conseguir ser pego, um dos returns do switch vai ser ativado e, diferentemente do break, ao ser alcançado ele vai sair do escopo do switch e da função para ir direto para o escopo aberto até onde a função foi chamada
+// }
+
+// console.log("Esse é o retorno da minha função : " + enviaMsg(null)); 
+
+// DIferença entre break e return
+
+// break simplesmente sai de dentro de um escopo menor, sai de dentro do escopo do switch, se der o return ele vaiu sair de dentro do escopo total e vai para dentro de quem chamou a função, ele sai direto para ir até o escopo aberto, vai para quem chamou a função
+
