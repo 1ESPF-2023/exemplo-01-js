8a4fad146da6fc00b96aa8292ebf5b6cb02c41c7
Migsrs123 <rm551239@fiap.com.br>
2023-04-10 09:40:35 -0300

    functions / switch case/ return/param

--- rm551239/js/scrip.js
+++ rm551239/js/scrip.js
@@ -26,21 +26,81 @@
 // console.log("Nome: " + nome)
 
 //capturar o elemento atravez do id utilizando uma finção do 
-const h1Element = document.getElementById("meu-titulo");
+// const h1Element = document.getElementById("meu-titulo");
     
-//Atrelar ou adicionar  evento de click do mouse ao elemento
-h1Element.addEventListener("click",()=>{
-    alert("Agora com super poderes!")
-})
+// // //Atrelar ou adicionar  evento de click do mouse ao elemento
+// h1Element.addEventListener("click",()=>{
+//     alert("Agora com super poderes!")
+// })
 
-//Altere a cor do fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
-//Coloque a cor de fundo na cor vermelha e a cor do texto na cor branca
+// //Altere a cor do fundo do elemento quando ocorrer a passagem do mouse sobre o elemento!
+// //Coloque a cor de fundo na cor vermelha e a cor do texto na cor branca
 
-h1Element.addEventListener("mouseover",()=>{
-    h1Element.style.backgroundColor = '#ff0000';
-    h1Element.style.color = '#fff';
-})
-h1Element.addEventListener("mouseout",()=>{
-    h1Element.style.backgroundColor = '#fff';
-    h1Element.style.color = '#000000';
-})
+// h1Element.addEventListener("mouseover",()=>{
+//     h1Element.style.backgroundColor = '#ff0000';
+//     h1Element.style.color = '#fff';
+// })
+// // h1Element.addEventListener("mouseout",()=>{
+// //     h1Element.style.backgroundColor = '#fff';
+// //     h1Element.style.color = '#000000';
+// // })
+
+// //h1Element.addEventListener("mouseout", alteraCorFundo)
+
+// //let msg = "Bom dia amigos"
+
+// function enviaMsg(msg){
+//     //Parametro recebido e sendo verificado
+//     //SWITCH CASE
+
+//     const pElement = document.getElementById("p-msg");
+
+//     switch (msg) {
+//         case undefined:
+//             console.log("Foi enviada uma mensagem vazia")
+//             pElement.textContent = "Foi enviada uma mensagem vazia";
+//             return "SUCESSO";
+//         case null:
+//             console.log("Foi enviada uma mensagem nula")
+//             pElement.textContent = "Foi enviada uma mensagem nula";
+//             return "SUCESSO";
+//         case "":
+//             console.log("Foi enviada uma mensagem em branco")
+//             pElement.textContent = "Foi enviada uma mensagem em branco";
+//             return "SUCESSO";
+//         case "Bom dia amigos":
+//             console.log("Bom dia amigos")
+//             pElement.textContent = "Bom dia amigos";
+//             return "SUCESSO";
+//         default:
+//             console.error("Ocorreu um erro na transmissão")
+//             pElement.textContent = "Ocorreu um erro na transmissão";
+//             return "SUCESSO";
+//     }
+
+// }
+
+// console.log("Esse é o retorno da minha função: " + enviaMsg(undefined));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
