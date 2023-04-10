0ae0edcca6e293b99881ef065b73396b2651e4ad
KauePastori <rm98501@fiap.com.br>
2023-04-10 09:40:27 -0300

    Functions/Switch case/return/parametro

--- rm98501/js/script.js
+++ rm98501/js/script.js
@@ -1,46 +1,58 @@
-//Executando um teste
 
-//Declarando uma variável e visualizando seu conteúdo.
+// capturar o elemento atravês e visualizando seu conteudo
 
-//Comentário de linha
+const h1Element = document.getElementById("meu_titulo")
 
-/**
- * Comentário de bloco
- */
+//Atrelar ou adicionar o evento de click do mouse ao elemento.
+h1Element.addEventListener("click",()=>{
+    alert("Agora com superpoderes!")
+})
 
-//3 tipos de dados inciais no var
-//undefined
-var tipo1   
-//null
-var tipo2 = null 
-//Vazio 
-var tipo3 = ""  
+//atrelar ou adicionar o evento de clique
+h1Element.addEventListener("mouseover",()=>{
+    h1Element.style.backgroundColor = "#ff0000";
+    h1Element.style.color = "#fff";
+})
+// h1Element.addEventListener("mouseout",()=>{
+//     h1Element.style.backgroundColor = "#fff";
+//     h1Element.style.color = "#000000";
+// })
 
-//Problemas ao declarar tudo com var:
-var nome = "Kaue"
-if(nome != ""){
-    let nome = "Pedro"
+//h1Element.addEventListener("mouseout",alteraCorFundo)
+
+//let msg = "Bom dia amigos!";
+
+function enviaMsg(msg) {
+
+    //Parâmetro recebido e sendo verificado
+    //SWITCH CASE
+
+    const pElement = document.getElementById("p-msg");
+
+    switch (msg) {
+        case undefined:
+            console.log("Foi enviada uma mensagem vazia!");
+            pElement.textContent = "Foi enviada uma mensagem vazia!";
+            return "Foi enviada uma mensagem vazia!";
+        case null:
+            console.log("Foi enviada uma mensagem nula!");
+            pElement.textContent = "Foi enviada uma mensagem nula!";
+            return "Foi enviada uma mensagem nula!";
+        case "":
+            console.log("Foi enviada uma mensagem em branco!");
+            pElement.textContent = "Foi enviada uma mensagem em branco!";
+            return "Foi enviada uma mensagem em branco!";;
+        case "Bom dia amigos":
+            console.log("Bom dia amigos!");
+            pElement.textContent = "Bom dia amigos!";
+            return  "Bom dia amigos!";
+        default:
+            console.error("Ocorreu um erro na transmissão!");
+            pElement.textContent = "Ocorreu um erro na transmissão!";
+            return "Ocorreu um erro na transmissão!"; 
+    }
+    return "SUCESSO!";
 }
 
-console.log("NOME :" + nome)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var nome = "Kaue"
-
-// document.write("O valor da variável.")
-// //Realizando uma concatenação 
-// document.write("NOME: " + nome);
+console.log("Esse é o retorno: " + enviaMsg(null))
 
